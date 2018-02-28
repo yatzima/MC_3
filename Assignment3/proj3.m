0de62a0911060f4b192be6d850381228b41db1cb
Justin Ma <justinmazk@gmail.com>
2018-02-28 17:26:46 +0100

    :))

--- Assignment3/proj3.m
+++ Assignment3/proj3.m
@@ -6,7 +6,9 @@ plot(T, 1:751)
 %Gibbs sampling
 samples = 2e4;
 burn_in = 4e3;
-rho = ones(d-1, 1);
+d = 3; 
+
+rho = 0.5*ones(d,1);
 psi = 2;
 tau = T;
 
@@ -15,11 +17,10 @@ t1 = 1658;
 td1 = 1980;
 
 %Initiate random t
-d = 4; 
 t = [t1 1700, 1801, 1850, td1];
 
 theta = gamrnd(2, psi);
-lambda = gamrn(2, theta, 1, d);
+lambda = gamrnd(2, theta, 1, d);
 
 thetaAll = zeros(samples, 1);
 lambdaAll = zeros(samples, d);
@@ -28,24 +29,19 @@ allt = zeros(samples, d + 1);
 for i = 1:burn_in
     theta = drawTheta(lambda, psi);
     lambda = drawLambda(theta, t, tau);
-    [t, accepted] = drawt(lambda, t, tau, rho)
+    [t, accepted] = drawt(lambda, t, tau, rho);
 end
 
+NTrials = 0;
+NAccepted = zeros(1,d-1);
+
 for i = 1:samples
-    
-    
-    %draw theta
-    d = length(lambda);
-    theta(i) = gamrnd(2*d + 2, psi + sum(lambda));
-    
-    n = cumsum(t <= tau);
-%    n = zeros(length(t) - 1, 1);
-%    for j = 1:(length(t)-1)
-%        n(j) = sum(t(j) <= tau) & (tau < (j+1));
-%    end
-    
-    %draw lambda
-    tDiff = t(2:end) - t(1:end-1);
-    lambda(i) = gamrnd(1, 2, tDiff+theta);
-    
+    theta = drawTheta(lambda, psi);
+    lambda = drawLambda(theta, t, T);
+    [t, accepted] = drawt(lambda, t, T, rho);
+    NTrials = NTrials + 1;
+    NAccepted = NAccepted + accepted;
+    allTheta(i, :) = theta;
+    allLambda(i, :) = lambda;
+    allt(i, :) = t;
 end
